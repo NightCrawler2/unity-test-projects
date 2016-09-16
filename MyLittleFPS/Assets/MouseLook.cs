749a05f2fd21cd3c9bfd4081bea93e0b005efaf0
Sergey Beketov <sergey.beketov@bsc-ideas.com>
2016-09-16 10:50:24 +0300

    test commit

---
+++ MyLittleFPS/Assets/MouseLook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+    public enum RotationAxes
+    {
+        MouseXnY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXnY;
+    public float sensitivityHor = 6.0f;
+    public float sensitivityVert = 6.0f;
+
+    public float maxVert = 45.0f;
+    public float minVert = -45.0f;
+
+    private float _rotationX = 0;
+    // Use this for initialization
+    void Start () {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null)
+        {
+            body.freezeRotation = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else if(axes == RotationAxes.MouseXnY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+	
+	}
+}
